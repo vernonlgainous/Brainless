30a63ec67f6ce438690308f3b35ffa1c1727801e
Mayur Mehta <mmehta10@ufl.edu>
2013-02-17 19:01:09 -0500

    worked on player health

--- Brainless/src/edu/ufl/brainless/Player.java
+++ Brainless/src/edu/ufl/brainless/Player.java
@@ -3,12 +3,57 @@ package edu.ufl.brainless;
 import android.graphics.Bitmap;
 
 public class Player extends Actor {
+	public int health; //health of player ranges from 0 to 100
+	public boolean isDead;
 	
 	private static final String TAG = Player.class.getSimpleName();
-
-	public Player(Bitmap texture, float x, float y) {
+	
+	public Player(Bitmap texture, float x, float y) { //default constructor
 		super(texture, x, y);
+		health = 100; //every player starts out with full health
+		isDead = false;
 		// TODO Auto-generated constructor stub
 	}
-
+	public Player(Bitmap texture, float x, float y, int health, boolean isDead) {		
+		super(texture, x, y);
+		this.isDead = isDead;
+		this.health = health;
+		// TODO Auto-generated constructor stub
+	}
+	
+	// Get Set methods
+	
+	public int getHealth(){
+		return health;
+	}
+	
+	public void setHealth(int health){
+		this.health = health;
+	}
+	
+	public boolean getIsDead(){
+		return isDead;
+	}
+	
+	public void setIsDead(boolean isDead){
+		this.isDead = isDead;
+	}
+	
+	//death method
+	public boolean isDead(){
+		if(this.isDead == true){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public void Death(){
+		if (this.isDead()){
+			// TODO execute death
+		}
+		
+	}
+	
 }
